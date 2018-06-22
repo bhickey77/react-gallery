ce1fa161427bee734055cabcf2e9597b854d6d2e
Bill Hickey <bhickey77@gmail.com>
2018-06-22 16:06:53 -0500

    added photos

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/car.jpg', description: 'Photo of a car taken at Glacier National Park.', likes: 0 },
+    { id: 3, path: 'images/sash.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 4, path: 'images/wheel.jpg', description: 'Photo of a car taken at Glacier National Park.', likes: 0 }
 ];
 
 module.exports = galleryItems;
